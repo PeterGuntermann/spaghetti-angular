9ef5c4e5332d2b1c1d97a676471e850f20b329b0
Peter Guntermann <pgu@posteo.de>
2023-09-21 22:39:38 +0200

    Add Bootstrap

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AgGridModule } from "ag-grid-angular";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CleanModule } from "./clean/clean.module";
@@ -9,14 +8,7 @@ import { DirtyModule } from "./dirty/dirty.module";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        DirtyModule,
-        CleanModule,
-        AgGridModule,
-        HttpClientModule,
-    ],
+    imports: [BrowserModule, AppRoutingModule, DirtyModule, CleanModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- demo/src/app/dirty/dirty.module.ts
+++ demo/src/app/dirty/dirty.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AgGridModule } from "ag-grid-angular";
+import { TooltipModule } from "ngx-bootstrap/tooltip";
 import { DirtyComponent } from "./dirty.component";
 
 @NgModule({
     declarations: [DirtyComponent],
-    imports: [CommonModule, AgGridModule],
+    imports: [CommonModule, AgGridModule, TooltipModule],
     exports: [DirtyComponent],
 })
 export class DirtyModule {}
